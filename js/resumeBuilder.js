434bdb312714fa4b1ccc2ade31bbff12730517ef
chris-hammersley <chris.hammersley@gmail.com>
2014-10-26 23:28:25 -0700

    Lesson 1 Complete

---
+++ js/resumeBuilder.js
@@ -0,0 +1,106 @@
+var bio = {
+    "name": "Chris Hammersley",
+    "role": "Director of E-Commerce",
+    "contacts" : {
+        "mobile": "206-734-4548",
+        "email": "chammers@gmail.com",
+        "github": "chris-hammersley",
+        "twitter": "mediazombi",
+        "location": "Seattle"
+        },
+    "welcomeMessage": "Hi. I'm a veteran in the field of taking complex data sets and making them actionable. Then I translate those actions into new front-end design UI.",
+    "skills": [
+        "E-Commerce", " Online Marketing", " SEO/SEM/PPC", " Google Analytics", " Data Visualization", " Email Nurturing"],
+    "pic": "images/bio-pic.jpg"
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "The New School",
+            "city": "New York, NY",
+            "degree": "MA",
+            "major": "Media Studies",
+            "dates": "2000-2003",
+            "url": "http://www.newschool.edu"
+        },
+        {
+            "name": "Loyola University",
+            "city": "Chicago, IL",
+            "degree": "BA",
+            "major": "Communication",
+            "dates": "1989-1993",
+            "url": "http://www.luc.edu"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front-end Web Development",
+            "school": "Udacity",
+            "dates": "2014-2015",
+            "url": "https://www.udacity.com/course/viewer#!/c-nd001"
+        },
+        {
+            "title": "Data Visualization Theory: A Pratical Introduction",
+            "school": "University of Washington",
+            "dates": "2014-2015",
+            "url": "http://www.pce.uw.edu/courses/data-visualization-theory.html"
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "Project 1",
+            "dates": "October, 2014",
+            "description": "Project 1 is an anagram app.",
+            "images": [
+                "images/1.jpg",
+                "images/2.jpg"
+            ]
+        },
+        {
+            "title": "Project 2",
+            "dates": "November, 2014",
+            "description": "Project 2 is an interactive resume.",
+            "images": [
+                "images/3.jpg",
+                "images/4.jpg"
+            ]
+        }
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "ClickSafety",
+            "title": "Director of E-Commerce &amp Online Marketing",
+            "location": "Walnut Creek, CA",
+            "dates": "2012 - Current",
+            "description": "Responsible for rapid growth of online sales channel."
+        },
+        {
+            "employer": "Ion Training",
+            "title": "President/Founder",
+            "location": "Seattle, WA",
+            "dates": "2008-2012",
+            "description": "Managed an educational-based creative content company using storytelling as foundation for connecting businesses &amp brands with their customers."
+        },
+        {
+            "employer": "BrickFactor",
+            "title": "President/CEO",
+            "location": "New York, NY",
+            "dates": "2006-2007",
+            "description": "Started a systems integration company focused on search technologies for online retail, technology &amp media industries."
+        },
+        {
+            "employer": "B&ampH Photo Video",
+            "title": "VP, New Media",
+            "location": "New York, NY",
+            "dates": "2003-2006",
+            "description": "Managed an online retail research &amp development team tasked with achieving long-term customer retention."
+        }
+    ]
+}
